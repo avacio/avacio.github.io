546d4a8e53db0b7eb6af045f50fb73f514c48046
Alexis Gervacio <lexigerv@gmail.com>
2017-09-25 06:41:19 -0700

    debugging

--- functions.js
+++ functions.js
@@ -4,14 +4,13 @@ var $document = $(document),
 
 $(document).ready(function() {
     $("#about-link").click(function(){
-   	$("#about").fadeToggle(1000);
-    $("#music").fadeToggle(1000);
+   	$("#about-bg").fadeToggle(1000);
     $("#work-nav").fadeToggle(1000);
-//  	$("#about2").toggleClass("active");
+  	$("#about2").toggleClass("active");
     $("body").toggleClass("body-active");
     $("#contact").toggleClass("contact-active");
   	$(this).text(function(i, text){
-      return text === "close x" ? "about me" : "close x";
+      return text === "close x" ? "" : "close x";
     });
   });
     
@@ -20,5 +19,20 @@ $(document).ready(function() {
 	event.preventDefault();
 	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 1200);
 	});
+
+ function goToAnchor() { 
+    hash = document.location.hash;
+    if (hash !="") {
+        setTimeout(function() {
+            if (location.hash) {
+                window.scrollTo(0, 0);
+                window.location.href = hash;
+            }
+        }, 1);
+    }
+    else {
+        return false;
+    }
+}
 });
 
