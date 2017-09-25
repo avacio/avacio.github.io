615f6d8590d2d70165e7b03ceb2fa6fac54c13c0
avacio <lexigerv@gmail.com>
2017-09-25 02:43:04 -0700

    added navigation

--- functions.js
+++ functions.js
@@ -2,22 +2,22 @@ var $document = $(document),
 		windowHeight = $(window).height(),
 		windowWidth = $(window).width();
 
-//$(document).ready(function() {
-//    $("#about-link").click(function(){
-//   	$("#about-bg").fadeToggle(1000);
-//    $("#work-nav").fadeToggle(1000);
+$(document).ready(function() {
+    $("#about-link").click(function(){
+   	$("#about-bg").fadeToggle(1000);
+    $("#work-nav").fadeToggle(1000);
 //  	$("#about2").toggleClass("active");
-//    $("body").toggleClass("body-active");
-//    $("#contact").toggleClass("contact-active");
-//  	$(this).text(function(i, text){
-//      return text === "close x" ? "about me" : "close x";
-//    });
-//  });
-//    
-//    
-//	$(".scroll").click(function(event){		
-//	event.preventDefault();
-//	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 1200);
-//	});
-//});
+    $("body").toggleClass("body-active");
+    $("#contact").toggleClass("contact-active");
+  	$(this).text(function(i, text){
+      return text === "close x" ? "about me" : "close x";
+    });
+  });
+    
+    
+	$(".scroll").click(function(event){		
+	event.preventDefault();
+	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 1200);
+	});
+});
 
