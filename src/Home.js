7b4d5295d0a082467bf0090a7a3a9b7b1bc11c80
avacio <lexigerv@gmail.com>
2025-08-11 15:11:45 -0700

    Improving load times on slow devices

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.css';
 import { Footer } from './components/Footer';
 import { NavigationPanel } from './components/NavigationPanel';
@@ -9,16 +8,31 @@ import { ProjectsView } from './components/ProjectsView';
 import { ProjectView } from './components/ProjectView';
 import { ProjectLinkPreview } from './components/ProjectLinkPreview';
 
+const AnimLoadState = Object.freeze({
+  UNLOADED: 0,
+  STATIC_LOADED: 1,
+  ANIM_LOADED: 2
+});
 
 function Home() {
   const [isWide, setIsWide] = useState(false);
-  const [isAnimLoaded, setAnimLoaded] = useState(0);
+  const [animLoadState, setAnimLoaded] = useState(AnimLoadState.UNLOADED);
+  const onAnimLoaded = () => {
+    setAnimLoaded(AnimLoadState.ANIM_LOADED);
+  }
+
+  const onStaticImageLoaded = () => {
+    if (animLoadState == AnimLoadState.UNLOADED) {
+      setAnimLoaded(AnimLoadState.STATIC_LOADED);
+    }
+  }
 
   useEffect(() => {
       // Handler to update window size state
       const handleResize = () => {
         setIsWide(window.innerWidth > 900);
       };
+    // console.log(`screen width: ${document.documentElement.clientWidth}`);
 
       handleResize();
       // Add event listener for 'resize'
@@ -29,33 +43,35 @@ function Home() {
         window.removeEventListener('resize', handleResize);
       };
 
-  }, []); // Empty dependency array ensures it runs only once
-
-  useEffect(() => {
-    document.getElementById('vidLoop').addEventListener('loadeddata', function () {
-      // Video is loaded and can be played
-      if (isAnimLoaded == 0) {
-        setAnimLoaded(1);
-      }
-      setTimeout(() => {
-        setAnimLoaded(2);
-      }, 3000);
-    }, false);
-  }, []);
+    }, []); // Empty dependency array ensures it runs only once
 
     return (
       <div className="App">
 
-        <div className={isAnimLoaded == 0 ? "loading-splash show" : isAnimLoaded == 1 ? "loading-splash hide" : "loading-splash displayNone"}><div className={isAnimLoaded == 0 ? "loader" : "displayNone"}></div></div>
+        <div className={animLoadState == AnimLoadState.UNLOADED ? "loading-splash show" : animLoadState == AnimLoadState.STATIC_LOADED ? "loading-splash hide" : "loading-splash displayNone"}><div className={animLoadState == AnimLoadState.UNLOADED ? "loader" : "displayNone"}></div></div>
 
         <div id="body" className={isWide ? 'limit1200' : 'limit400'}>
-
         <div className="main">
             <div className="nameTitle">alexis gervacio</div>
           <div className="biganimation">
               <div className="animation" id="loop" >
-                <video id="vidLoop" autoPlay="autoplay" loop="loop" muted="" playsInline loading="eager"
-                  alt="Animation of a girl rolling a rock up a mountain like Sisyphus."><source src="img/sisyphus.mp4" type="video/mp4" /></video>
+                {animLoadState < AnimLoadState.ANIM_LOADED &&
+                  <img src="img/sisyphus-transparent.png"
+                    className="staticAnimFrame"
+                    onLoad={onStaticImageLoaded}
+                    fetchPriority="high"
+                  />}
+
+                {animLoadState >= AnimLoadState.STATIC_LOADED && <img id="sisyphusAnim" key="img/sisyphus.gif"
+                  src={document.documentElement.clientWidth <= 576 ? "img/sisyphus-transparent-576w.gif" :
+                    document.documentElement.clientWidth <= 768 ?
+                      "img/sisyphus-transparent-768w.gif" :
+                      "img/sisyphus-transparent-1152w.gif"
+                  }
+
+                  type="image/gif"
+                  fetchPriority="low"
+                  onLoad={onAnimLoaded} />}
             </div>
           </div>
 
